5eddd54251e935a8bed32a0626007e48b0d4a23d
Hosein_Askari <101398814+Hosein-Askari@users.noreply.github.com>
2024-09-01 16:05:10 +0330

    Delete pose_landmark/level_4/ipynb_2 directory

--- pose_landmark/level_4/ipynb_2/Pose_Sample.py
+++
@@ -1,8 +0,0 @@
-class PoseSample(object):
-
-  def __init__(self, name, landmarks, class_name, embedding):
-    self.name = name
-    self.landmarks = landmarks
-    self.class_name = class_name
-
-    self.embedding = embedding
